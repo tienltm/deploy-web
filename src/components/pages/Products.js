560b9a269c6079620cf2f6a8cd579d47bb1b1609
ngocnpd <ngocnpd2901@gmail.com>
2022-12-16 22:55:45 +0700

    giao dien phan feedback

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,6 +1,52 @@
 import React from 'react';
 import '../../App.css';
+import { Link, Route, Router } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import './product.css';
 
-export default function Products() {
-  return <h1 className='products'>PRODUCTS</h1>;
+
+function Products() {
+  return (
+    <div className='feedbacks'>
+      <div className='feedback'>
+        <Link to = "/feedback">
+          <img src="https://i0.wp.com/crmviet.vn/wp-content/uploads/2021/01/feedback-art-sign-760-min.jpg?ssl=1" className='fb-img'></img>
+          <div className='fb-content'>Chỗ ni ghi nội dung sơ lược</div>
+        </Link>
+      </div>
+      <div className='feedback'>
+        <Link to = "/feedback">
+          <img src="https://i0.wp.com/crmviet.vn/wp-content/uploads/2021/01/feedback-art-sign-760-min.jpg?ssl=1" className='fb-img'></img>
+          <div className='fb-content'>Chỗ ni ghi nội dung sơ lược</div>
+        </Link>
+      </div>
+      <div className='feedback'>
+        <Link to = "/feedback">
+          <img src="https://i0.wp.com/crmviet.vn/wp-content/uploads/2021/01/feedback-art-sign-760-min.jpg?ssl=1" className='fb-img'></img>
+          <div className='fb-content'>Chỗ ni ghi nội dung sơ lược</div>
+        </Link>
+      </div>
+      <div className='feedback'>
+        <Link to = "/feedback">
+          <img src="https://i0.wp.com/crmviet.vn/wp-content/uploads/2021/01/feedback-art-sign-760-min.jpg?ssl=1" className='fb-img'></img>
+          <div className='fb-content'>Chỗ ni ghi nội dung sơ lược</div>
+        </Link>
+      </div>
+      <div className='feedback'>
+        <Link to = "/feedback">
+          <img src="https://i0.wp.com/crmviet.vn/wp-content/uploads/2021/01/feedback-art-sign-760-min.jpg?ssl=1" className='fb-img'></img>
+          <div className='fb-content'>Chỗ ni ghi nội dung sơ lược</div>
+        </Link>
+      </div>
+      <div className='feedback'>
+        <Link to = "/feedback">
+          <img src="https://i0.wp.com/crmviet.vn/wp-content/uploads/2021/01/feedback-art-sign-760-min.jpg?ssl=1" className='fb-img'></img>
+          <div className='fb-content'>Chỗ ni ghi nội dung sơ lược</div>
+        </Link>
+      </div>
+      
+    </div>
+    )
 }
+
+export default Products;
